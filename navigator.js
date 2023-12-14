fddb915a488193adc383d3d27ab0c4ffc104932d
Diego Martinez Franco <143146832+FastMartini@users.noreply.github.com>
2023-12-14 01:42:54 -0500

    Updated Symptom Navigator
    - Added a new javascript file for transition of new text with the press of a button (work in progress).
    
    - Completely terminated the binary tree algorithm; implementing seamless transition like text such as the one used by WebMD

---
+++ navigator.js
@@ -0,0 +1,8 @@
+const textElement = document.getElementById('display-text');
+const newText = 'What is your sex?';
+
+function changeText() {
+    // Add any transition effects here if needed
+
+    textElement.innerHTML = newText;
+}
